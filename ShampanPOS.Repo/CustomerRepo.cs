776802f3d75ce5c05fe81d90eefe9e40f21e16ea
DEV-1020\Rukaiya <rukaiya.akhter@symphonysoftt.com>
2026-01-31 17:55:18 +0600

    Add CustomerReport in Customer

--- ShampanPOS.Repo/CustomerRepo.cs
+++ ShampanPOS.Repo/CustomerRepo.cs
@@ -197,6 +197,24 @@ namespace ShampanPOS.Repo
                 throw e;
             }
         }
+        public ResultVM GetCustomerByCategory(CommonVM model)
+        {
+            try
+            {
+                HttpRequestHelper httpRequestHelper = new HttpRequestHelper();
+                AuthModel authModel = new AuthModel { token = ClaimNames.token };
+                #region Invoke API
+                var data = httpRequestHelper.PostData("api/Report/GetCustomerByCategory", authModel, JsonConvert.SerializeObject(model));
+                ResultVM result = JsonConvert.DeserializeObject<ResultVM>(data);
+                #endregion
+
+                return result;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
 
     }
 }
