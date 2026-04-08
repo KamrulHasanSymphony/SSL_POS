34bea93727c7deb6b873333255a46eefe575f8e9
DEV-1020\Rukaiya <rukaiya.akhter@symphonysoftt.com>
2026-04-08 19:05:05 +0600

    FiXed Purchase Report

--- ShampanPOS.Models/PurchaseVM.cs
+++ ShampanPOS.Models/PurchaseVM.cs
@@ -70,6 +70,7 @@ namespace ShampanPOS.Models
         public decimal TotalVAT { get; set; }
         public decimal GrandTotal { get; set; }
         public decimal PaidAmount { get; set; }
+        public decimal DueAmount { get; set; }
 
 
 
