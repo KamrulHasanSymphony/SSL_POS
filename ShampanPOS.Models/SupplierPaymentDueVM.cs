20a06c07ae7a292b6d74cd3195ac1d8bef83f87a
RakibulTusarSSL <rakibul.tusar@symphonysoftt.com>
2026-06-17 13:02:08 +0600

    add supplier payment due report

---
+++ ShampanPOS.Models/SupplierPaymentDueVM.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShampanPOS.Models
+{
+    public class SupplierPaymentDueVM
+    {
+        [DisplayName("Supplier")]
+        public int? SupplierId { get; set; }
+        [DisplayName("Supplier Code")]
+        public string? SupplierCode { get; set; }
+        [DisplayName("Supplier Name")]
+        public string? SupplierName { get; set; }
+        [DisplayName("Branch Code")]
+        public string? BranchCode { get; set; }
+        [DisplayName("Purchase Amount")]
+        public decimal PurchaseAmount { get; set; }
+        [DisplayName("Purchase Count")]
+        public int? PurchaseCount { get; set; }
+        [DisplayName("Total Payment Amount")]
+        public decimal TotalPaymentAmount { get; set; }
+        [DisplayName("Payment Count")]
+        public int? PaymentCount { get; set; }
+        [DisplayName("Last Payment Amount")]
+        public decimal LastPaymentAmount { get; set; }
+        [DisplayName("Due Amount")]
+        public decimal DueAmount { get; set; }
+        public int? BranchId { get; set; }
+        public int? CompanyId { get; set; }
+    }
+}
--- ShampanPOS.Repo/SaleRepo.cs
+++ ShampanPOS.Repo/SaleRepo.cs
@@ -475,5 +475,25 @@ namespace ShampanPOS.Repo
             }
         }
 
+
+        public ResultVM GetSupplierPaymentDueList(SupplierPaymentDueVM model)
+        {
+            try
+            {
+                HttpRequestHelper httpRequestHelper = new HttpRequestHelper();
+                AuthModel authModel = new AuthModel { token = ClaimNames.token };
+                var data = httpRequestHelper.PostData(
+                    "api/Report/GetSupplierPaymentDueList",
+                    authModel,
+                    JsonConvert.SerializeObject(model));
+                ResultVM result = JsonConvert.DeserializeObject<ResultVM>(data);
+                return result;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
     }
 }
