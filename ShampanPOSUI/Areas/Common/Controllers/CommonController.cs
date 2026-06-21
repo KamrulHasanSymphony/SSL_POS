58b5b1fa43b4e99b2ca98228a799e613470171dd
RakibulTusarSSL <rakibul.tusar@symphonysoftt.com>
2026-06-21 11:30:31 +0600

    feat: add null validation for BranchId and CompanyId in Get Purchase Order Modal

--- ShampanPOSUI/Areas/Common/Controllers/CommonController.cs
+++ ShampanPOSUI/Areas/Common/Controllers/CommonController.cs
@@ -1762,9 +1762,22 @@ namespace ShampanPOSUI.Areas.Common.Controllers
         {
             try
             {
+                if (Session["CurrentBranch"] == null)
+                {
+                    return Json(new { Status = "Error", Message = "Current Branch session is missing." }, JsonRequestBehavior.AllowGet);
+                }
+
+                if (Session["CompanyId"] == null)
+                {
+                    return Json(new { Status = "Error", Message = "Company session is missing." }, JsonRequestBehavior.AllowGet);
+                }
                 List<PurchaseOrderDataVM> lst = new List<PurchaseOrderDataVM>();
                 CommonVM param = new CommonVM();
                 param.Value = supplierId;
+
+                param.BranchId = Session["CurrentBranch"].ToString();
+                param.CompanyId = Session["CompanyId"].ToString();
+
                 ResultVM result = _repo.GetPurchaseOrderModal(param);
 
                 if (result.Status == "Success" && result.DataVM != null)
