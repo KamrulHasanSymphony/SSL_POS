a672bc346fce58d278465c279f01b1dda5a9706f
RakibulTusarSSL <rakibul.tusar@symphonysoftt.com>
2026-06-21 11:24:12 +0600

    feat: add null validation for BranchId and CompanyId in Get Purchase Modal

--- ShampanPOSUI/Areas/Common/Controllers/CommonController.cs
+++ ShampanPOSUI/Areas/Common/Controllers/CommonController.cs
@@ -666,9 +666,22 @@ namespace ShampanPOSUI.Areas.Common.Controllers
         {
             try
             {
+                if (Session["CurrentBranch"] == null)
+                {
+                    return Json(new { Status = "Error", Message = "Current Branch session is missing." }, JsonRequestBehavior.AllowGet);
+                }
+
+                if (Session["CompanyId"] == null)
+                {
+                    return Json(new { Status = "Error", Message = "Company session is missing." }, JsonRequestBehavior.AllowGet);
+                }
+
                 List<SupplierGroupVM> lst = new List<SupplierGroupVM>();
                 CommonVM param = new CommonVM();
                 param.Value = value;
+                param.BranchId = Session["CurrentBranch"].ToString();
+                param.CompanyId = Session["CompanyId"].ToString();
+
                 ResultVM result = _repo.GetSupplierGroupList(param);
 
                 if (result.Status == "Success" && result.DataVM != null)
@@ -1692,10 +1705,24 @@ namespace ShampanPOSUI.Areas.Common.Controllers
         {
             try
             {
+                if (Session["CurrentBranch"] == null)
+                {
+                    return Json(new { Status = "Error", Message = "Current Branch session is missing." }, JsonRequestBehavior.AllowGet);
+                }
+
+                if (Session["CompanyId"] == null)
+                {
+                    return Json(new { Status = "Error", Message = "Company session is missing." }, JsonRequestBehavior.AllowGet);
+                }
+
                 CommonVM param = new CommonVM
                 {
                     Value = supplierId
                 };
+
+                param.BranchId = Session["CurrentBranch"].ToString();
+                param.CompanyId = Session["CompanyId"].ToString();
+
 
                 ResultVM result = _repo.GetPurchaseModal(param);
 
