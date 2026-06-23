6c69e2c9de1de02be67f3f10f14c4f30b91ae9c5
RakibulTusarSSL <rakibul.tusar@symphonysoftt.com>
2026-06-23 11:58:13 +0600

    feat: add null validation for BranchId and CompanyId in Bank Account List

--- ShampanPOSUI/Areas/Common/Controllers/CommonController.cs
+++ ShampanPOSUI/Areas/Common/Controllers/CommonController.cs
@@ -1275,9 +1275,23 @@ namespace ShampanPOSUI.Areas.Common.Controllers
         {
             try
             {
+                if (Session["CurrentBranch"] == null)
+                {
+                    return Json(new { Status = "Error", Message = "Current Branch session is missing." }, JsonRequestBehavior.AllowGet);
+                }
+
+                if (Session["CompanyId"] == null)
+                {
+                    return Json(new { Status = "Error", Message = "Company session is missing." }, JsonRequestBehavior.AllowGet);
+                }
+
                 List<BankAccountVM> lst = new List<BankAccountVM>();
                 CommonVM param = new CommonVM();
                 param.Value = value;
+
+                param.BranchId = Session["CurrentBranch"].ToString();
+                param.CompanyId = Session["CompanyId"].ToString();
+
                 ResultVM result = _repo.GetBankAccountList(param);
 
                 if (result.Status == "Success" && result.DataVM != null)
