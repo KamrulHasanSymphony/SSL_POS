2a72584fd3e47d3cc6cf93c4f3e02b494c798d7a
RakibulTusarSSL <rakibul.tusar@symphonysoftt.com>
2026-06-23 10:58:58 +0600

    feat: add null validation for BranchId and CompanyId in Customer Group List

--- ShampanPOSUI/Areas/Common/Controllers/CommonController.cs
+++ ShampanPOSUI/Areas/Common/Controllers/CommonController.cs
@@ -620,9 +620,22 @@ namespace ShampanPOSUI.Areas.Common.Controllers
         {
             try
             {
+                if (Session["CurrentBranch"] == null)
+                {
+                    return Json(new { Status = "Error", Message = "Current Branch session is missing." }, JsonRequestBehavior.AllowGet);
+                }
+
+                if (Session["CompanyId"] == null)
+                {
+                    return Json(new { Status = "Error", Message = "Company session is missing." }, JsonRequestBehavior.AllowGet);
+                }
+
                 List<CustomerGroupVM> lst = new List<CustomerGroupVM>();
                 CommonVM param = new CommonVM();
                 param.Value = value;
+                param.BranchId = Session["CurrentBranch"].ToString();
+                param.CompanyId = Session["CompanyId"].ToString();
+
                 ResultVM result = _repo.GetCustomerGroupList(param);
 
                 if (result.Status == "Success" && result.DataVM != null)
