acdd639c28311df6cd5a6824445bf65a28ff7ab2
RakibulTusarSSL <rakibul.tusar@symphonysoftt.com>
2026-06-23 12:34:47 +0600

    feat: add null validation for BranchId and CompanyId in Get Table List

--- ShampanPOSUI/Areas/Common/Controllers/CommonController.cs
+++ ShampanPOSUI/Areas/Common/Controllers/CommonController.cs
@@ -1179,9 +1179,23 @@ namespace ShampanPOSUI.Areas.Common.Controllers
         {
             try
             {
+                if (Session["CurrentBranch"] == null)
+                {
+                    return Json(new { Status = "Error", Message = "Current Branch session is missing." }, JsonRequestBehavior.AllowGet);
+                }
+
+                if (Session["CompanyId"] == null)
+                {
+                    return Json(new { Status = "Error", Message = "Company session is missing." }, JsonRequestBehavior.AllowGet);
+                }
+
                 List<TableInfoVM> lst = new List<TableInfoVM>();
                 CommonVM param = new CommonVM();
                 param.Value = value;
+                param.BranchId = Session["CurrentBranch"].ToString();
+                param.CompanyId = Session["CompanyId"].ToString();
+
+
                 ResultVM result = _repo.GetTableList(param);
 
                 if (result.Status == "Success" && result.DataVM != null)
